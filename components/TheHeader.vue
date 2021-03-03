c970f353242f0fa485391b84caf98f374b7d9cac
Alex Malikov <alex.malikov94@gmail.com>
2021-03-03 14:46:44 +0500

    The start design Job Portal

---
+++ components/TheHeader.vue
@@ -0,0 +1,12 @@
+<template>
+    <header class="py-6 flex items-center justify-between">
+        <nuxt-link :to="{ name: 'index'}" exact-active-class="text-blue-500">Jobs</nuxt-link>
+        <div class="-mx-4 flex item-center flex-wrap">
+            <span class="mx-4 font-bold" exact-active-class="text-blue-500">Alex</span>
+            <nuxt-link :to="{ name: 'login'}" class="mx-4" exact-active-class="text-blue-500">Login</nuxt-link>
+            <a href="" class="mx-4" exact-active-class="text-blue-500">Manage Listings</a>
+            <a href="" class="mx-4" exact-active-class="text-blue-500">Logout</a>
+            <a href="" class="mx-4" exact-active-class="text-blue-500">Post a job</a>
+        </div>
+    </header>
+</template>
